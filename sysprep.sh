c9dc75772769fec0473bfeca8e8bf19d92962891
hhalibo <gao13903408669@gmail.com>
2025-03-15 21:10:23 +0800

    Update sysprep.sh

--- sysprep.sh
+++ sysprep.sh
@@ -4,28 +4,18 @@ echo
 echo "Change the Repositories to Tsinghua"
 echo
 
-sed -i.bak 's#http://apt.armbian.com#https://mirrors.tuna.tsinghua.edu.cn/armbian#g' /etc/apt/sources.list.d/armbian.list;
+#sed -i.bak 's#http://apt.armbian.com#https://mirrors.tuna.tsinghua.edu.cn/armbian#g' /etc/apt/sources.list.d/armbian.list;
 
 mv /etc/apt/sources.list /etc/apt/sources.list.orig;
 
 cat >> /etc/apt/sources.list <<'EOF'
-deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm main contrib non-free non-free-firmware
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm main contrib non-free non-free-firmware
-
-deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-updates main contrib non-free non-free-firmware
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-updates main contrib non-free non-free-firmware
-
-deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-backports main contrib non-free non-free-firmware
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-backports main contrib non-free non-free-firmware
-
-deb http://security.debian.org/ bookworm-security main contrib non-free non-free-firmware
-# deb-src http://security.debian.org/ bookworm-security main contrib non-free non-free-firmware
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye main 
 EOF
 
 apt update;
 apt upgrade -y;
 apt install mosquitto-clients -y;
-curl -L https://github.com/docker/compose/releases/download/v2.29.7/docker-compose-linux-aarch64 -o /usr/local/bin/docker-compose;
+curl -L https://github.com/docker/compose/releases/download/v2.33.1/docker-compose-linux-aarch64 -o /usr/local/bin/docker-compose;
 chmod +x /usr/local/bin/docker-compose;
 
 cat >> /root/docker.sh <<'EOF'
@@ -38,39 +28,29 @@ echo
 
 
 echo "Running apt-get preparation"
+# Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get upgrade -y
-
-
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-
-
-echo "add the GPG key for the official Docker repository to your system"
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 
 echo "Add the Docker repository to APT sources"
-sudo add-apt-repository \
-   "deb [arch=arm64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 
 echo "Redo apt update"
 sudo apt update
-sudo apt install -y avahi-daemon avahi-discover libnss-mdns libavahi-compat-libdnssd-dev g++ gcc jq socat
-
-
-echo "Make sure you are about to install from the Docker repo instead of the default debian repo"
-apt-cache policy docker-ce
-
 
 sleep 5
 
-
 echo "Install Docker now"
-sudo apt install docker-ce -y
-#sudo apt-get install docker-ce=5:26.1.4-1~debian.11~bookworm docker-ce-cli=5:26.1.4-1~debian.11~bookworm containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#sudo apt-get install docker-ce=5:26.1.4-1~debian.11~bullseye docker-ce-cli=5:26.1.4-1~debian.11~bullseye containerd.io
 
 
 echo
