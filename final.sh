4469ada9849563807a0ff54bf588a3888b680c6c
hhalibo <gao13903408669@gmail.com>
2025-03-15 21:13:38 +0800

    Update final.sh

--- final.sh
+++ final.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+usermod -aG sudo roger;
+apt --fix-broken install;
 
 echo
 echo "Install Final Step for S905L3A"
 echo
 
 apt --fix-broken install;
-sudo dpkg -i  os-agent_1.6.0_linux_aarch64.deb;
+sudo dpkg -i  os-agent_1.7.1_linux_aarch64.deb;
 wget https://github.com/home-assistant/supervised-installer/releases/download/1.8.0/homeassistant-supervised.deb;
 dpkg -i homeassistant-supervised.deb
